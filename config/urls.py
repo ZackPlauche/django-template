f1e0c559ef7a7b2f5f6f9502e7c0a88dd2ff6d2f
Zack Plauché <zackknowspython@gmail.com>
2021-04-04 17:03:20 +0300

    Add basic robots.txt

--- config/urls.py
+++ config/urls.py
@@ -2,9 +2,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('robots.txt', TemplateView.as_view(template_name='robots.txt', content_type='text/plain')),
 ]
 
 if settings.DEBUG: 
